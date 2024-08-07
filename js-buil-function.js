1ed3ba6612883ff2e80afdfd0f44f2086747dc76
MD Minarul Islam <wdminarul>
2024-08-07 15:48:04 +0600

    js-build-function

---
+++ js-buil-function.js
@@ -0,0 +1,19 @@
+// find a maximum number 
+
+const max =Math.max(45,89,23,45,67,84,988,235,78,32,14,567,85);
+console.log(max)
+//find a minimum nuber from array
+const min =Math.min(45,89,23,45,67,84,988,235,78,32,14,567,85);
+console.log(min)
+//Round a number from intiger 
+console.log(Math.round(5.6))
+// PI number
+console.log(Math.PI)
+//random number generate 
+console.log(Math.random()*20);
+//ceiling a intiger number 
+console.log(Math.ceil(3.11))
+//flooring a intiger number 
+console.log(Math.floor(9.9999))
+//Alwys abs is show an positive number 
+console.log(Math.abs(8-10))
